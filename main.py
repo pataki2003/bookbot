0ae679a617c29012e8ba1d41548c56d237a1515e
pataki2003 <patakiattila987@gmail.com>
2025-12-14 12:13:27 +0100

    Adding file reading to main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    booktext = get_book_text("books/frankenstein.txt")
+    print(booktext)
+
+main()
+
